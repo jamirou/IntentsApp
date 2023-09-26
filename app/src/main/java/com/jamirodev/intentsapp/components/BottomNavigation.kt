7efde180ac81752597cad6a4fc196ea124b2fef5
Jamiro <jamirocristian15@gmail.com>
2023-09-26 10:49:34 -0300

    adding homeview components

---
+++ app/src/main/java/com/jamirodev/intentsapp/components/BottomNavigation.kt
@@ -0,0 +1,2 @@
+package com.jamirodev.intentsapp.components
+
--- app/src/main/java/com/jamirodev/intentsapp/views/HomeView.kt
+++ app/src/main/java/com/jamirodev/intentsapp/views/HomeView.kt
@@ -1,2 +1,28 @@
 package com.jamirodev.intentsapp.views
 
+import Routes
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.jamirodev.intentsapp.navigation.NavManager
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeView() {
+    val navController = rememberNavController()
+    val navigationRoutes = listOf(
+        Routes.PhoneView,
+        Routes.SmsView,
+        Routes.EmailView
+    )
+    Scaffold(
+        bottomBar = {
+
+        }
+    ) {
+        NavManager(navController)
+    }
+}
