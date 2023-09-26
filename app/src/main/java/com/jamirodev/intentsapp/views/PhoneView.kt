07ccf27ea2f572933f9d8271b59db0df7e4e217f
Jamiro <jamirocristian15@gmail.com>
2023-09-26 11:15:14 -0300

    added vertical grid for calls

--- app/src/main/java/com/jamirodev/intentsapp/views/PhoneView.kt
+++ app/src/main/java/com/jamirodev/intentsapp/views/PhoneView.kt
@@ -1,9 +1,39 @@
 package com.jamirodev.intentsapp.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PhoneView() {
-    Text(text = "Phone View")
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Phone", fontSize = 50.sp, fontWeight = FontWeight.Bold)
+        val items = listOf(
+            "1", "2", "3",
+            "4", "5", "6",
+            "7", "8", "9",
+            "*", "0", "#"
+        )
+        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+            items(items) { item ->
+                Text(text = item)
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
