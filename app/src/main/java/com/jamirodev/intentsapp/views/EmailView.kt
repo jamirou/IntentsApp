5aa15a9d8966fe91b95b72db53dfad519b37e70f
Jamiro <jamirocristian15@gmail.com>
2023-09-26 12:19:48 -0300

    added mail intent

--- app/src/main/java/com/jamirodev/intentsapp/views/EmailView.kt
+++ app/src/main/java/com/jamirodev/intentsapp/views/EmailView.kt
@@ -1,10 +1,84 @@
 package com.jamirodev.intentsapp.views
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmailView() {
-    Text(text = "Email View")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var email by remember { mutableStateOf("") }
+        var subject by remember { mutableStateOf("") }
+        var message by remember { mutableStateOf("") }
+        val context = LocalContext.current
+        Text(text = "Email", fontSize = 50.sp, fontWeight = FontWeight.Bold)
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        OutlinedTextField(
+            value = subject,
+            onValueChange = { subject = it },
+            label = { Text(text = "subject") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        OutlinedTextField(
+            value = message,
+            onValueChange = { message = it },
+            label = { Text(text = "message") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(10.dp)
+        )
+
+        Button(onClick = {
+            val intent = Intent(Intent.ACTION_SEND)
+            val emailAddress = arrayOf(email)
+            intent.putExtra(Intent.EXTRA_EMAIL, emailAddress)
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "message/rfc822"
+            context.startActivity(Intent.createChooser(intent, "Email Client:"))
+        }) {
+            Text(text = "Send")
+        }
+
+    }
 }
